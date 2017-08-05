191a2b6f3552946acc391dc995fe5ee15f5f780d
martrinity <martiloca_4@hotmail.com>
2017-08-04 20:42:53 -0400

    valida seleccion

--- js/main.js
+++ js/main.js
@@ -42,4 +42,31 @@ function validarMail(){
     }
   }
   validarMail();
+
+  function contrasena(){
+    var passwors = $('#input-password').val();
+    if(password === null || password.length === 0){
+      $('.form-group:eq(0)').append('<span>Debe ingresar información requerida</span>');
+      return false;
+    }else if (password.length < 6 ) {
+      $('.form-group:eq(0)').append('<span>La contraseña valida debe tener al menos seis caracteres</span>');
+      return false;
+    }else if(password === 'passwordword' || password === '123456' || password === '098754'){
+      $('.form-group:eq(0)').append('<span>Contraseña inválida, ingrese una contraseña distinta a "123456"</span>');
+      return false;
+    }else{
+      return true;
+    }
+  }
+  contrasena();
+//*Valida que el usuario seleccione una de las opciones*/
+  function selectOpcion(){
+    var opcion = $('select').val();
+    if(opcion == 0){
+      $('.form-group:eq(1)').append('<span>Debe seleccionar una opción para continuar</span>');
+      return false;
+    }
+  }
+  selectOpcion();
+
 }
